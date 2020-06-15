ff22358db08937920687d453e4e0f8ffa6e41cce
Wataru Ono <wtr0414@gmail.com>
2020-06-15 17:51:25 +0900

    [add]簡易的な会社詳細画面の実装

---
+++ src/views/Detail.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>hello</h1>
+    <div class="company-name">
+      会社名：{{company.name}}
+    </div>
+    <div class="company-description">
+      説明：{{company.description}}
+    </div>
+  </div>
+</template>
+
+<script>
+import { db } from '../main'
+
+export default {
+  data () {
+    return {
+      company: {}
+    }
+  },
+  firestore () {
+    return {
+      company: db.collection('companies').doc(this.$route.params.id)
+    }
+  }
+}
+</script>

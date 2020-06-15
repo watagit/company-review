057fc0aaf0dc6d548d3d889cc37e677bd6365aac
Wataru Ono <wtr0414@gmail.com>
2020-06-15 10:54:30 +0900

    [add]会社の一覧を表示

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <dir class="list">
+      <li v-for="company in companies" :key="company.id" class="item">
+        {{company.name}}
+      </li>
+    </dir>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { db } from '../main'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+  data () {
+    return {
+      companies: []
+    }
+  },
+  firestore () {
+    return {
+      companies: db.collection('companies')
+    }
   }
 }
 </script>
